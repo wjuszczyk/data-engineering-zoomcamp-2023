159049b051867160ba456e6c55a26ddabc2a85cb
Wojciech Juszczyk <wojciech.juszczyk@gmail.com>
2023-02-07 02:04:39 +0100

    cleanups, multiple changes

--- week2_sandbox/ETL_GCP_Prefect/etl_web_to_gcs.py
+++ week2_sandbox/ETL_GCP_Prefect/etl_web_to_gcs.py
@@ -1,30 +1,27 @@
-from pathlib import Path
+import os
 import pandas as pd
+from pathlib import Path
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 #from random import randint
-import os
-from prefect_gcp.cloud_storage import GcsBucket
 
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
-    """Read taxi data from web into pandas DataFram"""
+    """Read taxi data from web into pandas DataFrame"""
     # if randint(0, 1) > 0:
     #     raise Exception
-
     df = pd.read_csv(dataset_url)
     return df
 
 @task(log_prints=True)
 def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
-    """Fix dtype issues"""
+    """Fix dtype issues depending on name of dataset"""
     if color == "green":
         df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
         df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
     elif color == "yellow":
         df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
         df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
-
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -33,40 +30,37 @@ def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
+    # if there's no directory structure, make ones
     if not os.path.isdir(f"data/{color}"):
         os.makedirs(f"data/{color}", exist_ok=True)
-
     path = Path(f"data/{color}/{dataset_file}.parquet")
-    
     df.to_parquet(path, compression="gzip")
     return path  
 
 @task()
 def write_gcs(path: Path) -> None:
     """Uploading local parquet file to GCS"""
-    # change path to posix type
+    # change path to posix type, requires prefect-gcp[cloud_storage]==0.2.4 (fix Windows double backslashes to slashes)
     path = Path(path).as_posix()
     gcp_cloud_storage_bucket_block = GcsBucket.load("zoom-gcs")
     gcp_cloud_storage_bucket_block.upload_from_path(
         from_path=f"{path}",
         to_path=f"{path}"
     )
-    pass
 
 @flow()
 def etl_web_to_gcs() -> None:
     """The main ETL function"""
     color = "green"
     year = 2020
+    # color = "yellow"    
+    # year = 2021
     month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
-
     df = fetch(dataset_url)
     df_clean = clean(df, color)
-
     path = write_local(df_clean, color, dataset_file)
-    # print(f"Path outside write_local is: {path}")
     write_gcs(path)
 
 if __name__ == "__main__":
