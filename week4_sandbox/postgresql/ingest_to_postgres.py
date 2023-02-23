5889b60e211c2cd7206713ca589217084abf58e5
Wojciech Juszczyk <wojciech.juszczyk@gmail.com>
2023-02-24 00:53:47 +0100

    adding ingesting of csv.gz files

--- week4_sandbox/postgresql/ingest_to_postgres.py
+++ week4_sandbox/postgresql/ingest_to_postgres.py
@@ -24,85 +24,96 @@ def main(params):
     engine = create_engine(
         f'postgresql://{user}:{password}@{host}:{port}/{database}')
 
-    path = Path(params.path).glob('**/*.parquet')
+    path = Path(params.path).glob('**/*')
     list_of_files = [x for x in path if x.is_file()]
 
     f_index = 1
 
     for file in list_of_files:
-        print(f"""
-* file: {file.resolve()} ({f_index}/{len(list_of_files)})
-  size: {file.stat().st_size / 1024 / 1024} MB
-- DB: {database}
-- schema: {table_name}
-            """)
+        print(f"* file: {file.resolve()} ({file.stat().st_size / 1024 / 1024:.2f} MB) ({f_index}/{len(list_of_files)}) -> DB: {database}, schema: {table_name}\n")
 
         t_start = time()
 
-        parquet_file = pq.ParquetFile(file)
-        parquet_size = parquet_file.metadata.num_rows
-        index = 65536
+        if file.name.endswith('parquet'):
+            parquet_file = pq.ParquetFile(file)
+            parquet_size = parquet_file.metadata.num_rows
+            index = 65536
 
-        for iterator in parquet_file.iter_batches(use_threads=True):
-            if index > parquet_size:
-                index = parquet_size
-            print(
-                f"Ingesting {index} out of {parquet_size} rows ({index / parquet_size:.0%})")
+            for iterator in parquet_file.iter_batches(use_threads=True):
+                if index > parquet_size:
+                    index = parquet_size
 
-            t_iter = time()
+                data_frame = iterator.to_pandas()
 
-            #data_frame = pd.read_parquet(file)
-            data_frame = iterator.to_pandas()
-            index += 65535
-            print("- starting types setting")
+                if "green" in file.name:
+                    data_frame['lpep_pickup_datetime'] = pd.to_datetime(
+                        data_frame['lpep_pickup_datetime'])
+                    data_frame['lpep_dropoff_datetime'] = pd.to_datetime(
+                        data_frame['lpep_dropoff_datetime'])
+                    data_frame["trip_type"] = data_frame["trip_type"].astype(
+                        'Int64')
+                    data_frame["VendorID"] = data_frame["VendorID"].astype(
+                        'Int64')
+                    data_frame["RatecodeID"] = data_frame["RatecodeID"].astype(
+                        'Int64')
+                    data_frame["PULocationID"] = data_frame["PULocationID"].astype(
+                        'Int64')
+                    data_frame["DOLocationID"] = data_frame["DOLocationID"].astype(
+                        'Int64')
+                    data_frame["passenger_count"] = data_frame["passenger_count"].astype(
+                        'Int64')
+                    data_frame["payment_type"] = data_frame["payment_type"].astype(
+                        'Int64')
 
-            if "green" in file.name:
-                data_frame['lpep_pickup_datetime'] = pd.to_datetime(
-                    data_frame['lpep_pickup_datetime'])
-                data_frame['lpep_dropoff_datetime'] = pd.to_datetime(
-                    data_frame['lpep_dropoff_datetime'])
-                data_frame["trip_type"] = data_frame["trip_type"].astype(
-                    'Int64')
-                data_frame["VendorID"] = data_frame["VendorID"].astype('Int64')
-                data_frame["RatecodeID"] = data_frame["RatecodeID"].astype(
-                    'Int64')
-                data_frame["PULocationID"] = data_frame["PULocationID"].astype(
-                    'Int64')
-                data_frame["DOLocationID"] = data_frame["DOLocationID"].astype(
-                    'Int64')
-                data_frame["passenger_count"] = data_frame["passenger_count"].astype(
-                    'Int64')
-                data_frame["payment_type"] = data_frame["payment_type"].astype(
-                    'Int64')
-                #print("\t- completed setting types for green data.")
+                if "yellow" in file.name:
+                    data_frame['tpep_pickup_datetime'] = pd.to_datetime(
+                        data_frame['tpep_pickup_datetime'])
+                    data_frame['tpep_dropoff_datetime'] = pd.to_datetime(
+                        data_frame['tpep_dropoff_datetime'])
+                    data_frame["VendorID"] = data_frame["VendorID"].astype(
+                        'Int64')
+                    data_frame["RatecodeID"] = data_frame["RatecodeID"].astype(
+                        'Int64')
+                    data_frame["PULocationID"] = data_frame["PULocationID"].astype(
+                        'Int64')
+                    data_frame["DOLocationID"] = data_frame["DOLocationID"].astype(
+                        'Int64')
+                    data_frame["passenger_count"] = data_frame["passenger_count"].astype(
+                        'Int64')
+                    data_frame["payment_type"] = data_frame["payment_type"].astype(
+                        'Int64')
 
-            if "yellow" in file.name:
-                data_frame['tpep_pickup_datetime'] = pd.to_datetime(
-                    data_frame['tpep_pickup_datetime'])
-                data_frame['tpep_dropoff_datetime'] = pd.to_datetime(
-                    data_frame['tpep_dropoff_datetime'])
-                data_frame["VendorID"] = data_frame["VendorID"].astype('Int64')
-                data_frame["RatecodeID"] = data_frame["RatecodeID"].astype(
-                    'Int64')
-                data_frame["PULocationID"] = data_frame["PULocationID"].astype(
-                    'Int64')
-                data_frame["DOLocationID"] = data_frame["DOLocationID"].astype(
-                    'Int64')
-                data_frame["passenger_count"] = data_frame["passenger_count"].astype(
-                    'Int64')
-                data_frame["payment_type"] = data_frame["payment_type"].astype(
-                    'Int64')
-                #print("\t- completed setting types for yellow data.")
+                print(
+                    f"Ingesting {index} out of {parquet_size} rows")
+                data_frame.to_sql(name=table_name, con=engine,
+                                  if_exists='append')
+                index += 65535
+            print(f"* Ingested, it took {time() - t_start:.1f} seconds\n")
 
-            print("- completed setting types data.")
-
-            #print(f"* ingesting {file.resolve()} to DB: {database}, schema: {table_name}")
+        elif file.name.endswith("csv.gz"):
+            chunksize = 100000
+            index = chunksize
+            csv_size = pd.read_csv(file).shape[0]
+            df_iter = pd.read_csv(file, iterator=True,
+                                  chunksize=chunksize, low_memory=False)
+            data_frame = next(df_iter)
 
             data_frame.to_sql(name=table_name, con=engine, if_exists='append')
 
-            print(
-                f"* Ingested, it took {time() - t_iter:.1f} seconds\n")
-        print(f"*** Total ingestion time {time() - t_start:.1f} seconds")
+            while True:
+                try:
+                    if index > csv_size:
+                        index = csv_size
+                    print(
+                        f"Ingesting {index} out of {csv_size} rows")
+                    data_frame = next(df_iter)
+                    data_frame.to_sql(
+                        name=table_name, con=engine, if_exists='append')
+                    index += chunksize
+                except StopIteration:
+                    print('Reached end of csv file.')
+                    break
+            print(f"* Ingested, it took {time() - t_start:.1f} seconds\n")
         f_index += 1
 
 
