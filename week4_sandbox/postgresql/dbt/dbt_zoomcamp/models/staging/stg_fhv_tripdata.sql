4b35e2de3b351304d9a8e5de9e81ce762b329993
Wojciech Juszczyk <wojciech.juszczyk@gmail.com>
2023-02-27 23:11:33 +0100

    adding models to postgesql dbt

--- week4_sandbox/postgresql/dbt/dbt_zoomcamp/models/staging/stg_fhv_tripdata.sql
+++ week4_sandbox/postgresql/dbt/dbt_zoomcamp/models/staging/stg_fhv_tripdata.sql
@@ -1,4 +1,13 @@
-{ { config(materialized = 'view') } }
-select cast() as,
-	from { { source('staging', 'fhv_2019') } }
-limit 100
+{{ config(materialized='view') }}
+
+select
+    {{ dbt_utils.surrogate_key(["old_index", "pickup_datetime"]) }} as tripid,
+    cast("old_index"as integer) as old_index,
+    cast("dispatching_base_num" as text) as dispatching_base_num,
+    cast("pickup_datetime" as timestamp) as pickup_datetime,
+    cast("dropOff_datetime" as timestamp) as dropoff_datetime,
+    cast("PUlocationID" as numeric) as pulocationid,
+    cast("DOlocationID" as numeric) as dolocationid,
+    cast("SR_Flag" as numeric) as sr_flag,
+    cast("Affiliated_base_number" as text) as affiliated_base_number
+from {{ source('staging', 'fhv_2019') }}
--- week4_sandbox/postgresql/dbt/dbt_zoomcamp/models/staging/stg_yellow_tripdata.sql
+++ week4_sandbox/postgresql/dbt/dbt_zoomcamp/models/staging/stg_yellow_tripdata.sql
@@ -3,41 +3,41 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+    row_number() over(partition by "VendorID", "tpep_pickup_datetime") as rn
   from {{ source('staging','yellow_trips_all') }}
-  where vendorid is not null 
+  where "VendorID" is not null 
 )
 select
    -- identifiers
-    {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
-    cast(vendorid as integer) as vendorid,
-    cast(ratecodeid as integer) as ratecodeid,
-    cast(pulocationid as integer) as  pickup_locationid,
-    cast(dolocationid as integer) as dropoff_locationid,
+    {{ dbt_utils.surrogate_key(['"VendorID"', '"tpep_pickup_datetime"']) }} as tripid,
+    cast("VendorID" as integer) as vendorid,
+    cast("RatecodeID" as integer) as ratecodeid,
+    cast("PULocationID" as integer) as  pickup_locationid,
+    cast("DOLocationID" as integer) as dropoff_locationid,
     
     -- timestamps
-    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast("tpep_pickup_datetime" as timestamp) as pickup_datetime,
+    cast("tpep_dropoff_datetime" as timestamp) as dropoff_datetime,
     
     -- trip info
-    store_and_fwd_flag,
-    cast(passenger_count as integer) as passenger_count,
-    cast(trip_distance as numeric) as trip_distance,
+    "store_and_fwd_flag",
+    cast("passenger_count" as integer) as passenger_count,
+    cast("trip_distance" as numeric) as trip_distance,
     -- yellow cabs are always street-hail
-    1 as trip_type,
+    1 as "trip_type",
     
     -- payment info
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
+    cast("fare_amount" as numeric) as fare_amount,
+    cast("extra" as numeric) as extra,
+    cast("mta_tax" as numeric) as mta_tax,
+    cast("tip_amount" as numeric) as tip_amount,
+    cast("tolls_amount" as numeric) as tolls_amount,
     cast(0 as numeric) as ehail_fee,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type,
-    {{ get_payment_type_description('payment_type') }} as payment_type_description, 
-    cast(congestion_surcharge as numeric) as congestion_surcharge
+    cast("improvement_surcharge" as numeric) as improvement_surcharge,
+    cast("total_amount" as numeric) as total_amount,
+    cast("payment_type" as integer) as payment_type,
+    {{ get_payment_type_description("payment_type") }} as payment_type_description, 
+    cast("congestion_surcharge" as numeric) as congestion_surcharge
 from tripdata
 where rn = 1
 
