ba67b11094c99c25c1a26897b6453e4ca9fd744b
Wojciech Juszczyk <wojciech.juszczyk@gmail.com>
2023-03-01 02:22:00 +0100

    syntax correction

--- week4_sandbox/postgresql/dbt/dbt_zoomcamp/models/core/fhv_monthly_count.sql
+++ week4_sandbox/postgresql/dbt/dbt_zoomcamp/models/core/fhv_monthly_count.sql
@@ -2,7 +2,7 @@
 
 with trips_data as (select * from {{ ref("fhv_fact_trips") }})
 select
-    date_trunc('month', pickup_datetime) as month, count(tripid) as total_monthly_trips,
+    date_trunc('month', pickup_datetime) as month, count(tripid) as total_monthly_trips
 from trips_data
 group by 1
 order by 2 desc
